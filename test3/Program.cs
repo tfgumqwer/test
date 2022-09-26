23a462d0f1739e3eff2873d4dffb9f1dfeb6fd48
tfgumqwer <114394803+tfgumqwer@users.noreply.github.com>
2022-09-26 11:26:52 +0200

    3.0

---
+++ test3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("asd");
+          int a =   Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(asdasd);
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
