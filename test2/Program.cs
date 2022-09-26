40a0065d260e7dc67fc2ff2586a40ac5d552c55d
tfgumqwer <114394803+tfgumqwer@users.noreply.github.com>
2022-09-26 11:22:50 +0200

    2.0

---
+++ test2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("adj meg egy szamot");
+            Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("adj meg még egy szamot");
+            Convert.ToInt32(Console.ReadLine());
+
+            Console.Readkey();
+        }
+
+    }
+}
+}
